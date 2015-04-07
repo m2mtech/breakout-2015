83b6ed7087bc3d10605341dac2b4bd86b1a67fad
m2m tech <tech@m2m.at>
2015-04-07 13:50:35 +0200

    step #2 adding the paddle

--- Breakout/ViewController.swift
+++ Breakout/ViewController.swift
@@ -11,16 +11,35 @@ import UIKit
 class ViewController: UIViewController {
 
     struct Constants {
-        static let BallSize: CGFloat = 40.0
+        static let BallRadius: CGFloat = 20.0
         static let BallColor = UIColor.blueColor()
         
+        static let PaddleSize = CGSize(width: 80.0, height: 20.0)
+        static let PaddleCornerRadius: CGFloat = 5.0
+        static let PaddleColor = UIColor.greenColor()
+        
         static let BoxPathName = "Box"
+        static let PaddlePathName = "Paddle"
     }
     
     @IBOutlet weak var gameView: UIView!
 
     let breakout = BreakoutBehavior()
     lazy var animator: UIDynamicAnimator = { UIDynamicAnimator(referenceView: self.gameView) }()
+    
+    lazy var paddle: UIView = {
+        let paddle = UIView(frame: CGRect(origin: CGPoint(x: -1, y: -1), size: Constants.PaddleSize))
+        paddle.backgroundColor = Constants.PaddleColor
+        paddle.layer.cornerRadius = Constants.PaddleCornerRadius
+        paddle.layer.borderColor = UIColor.blackColor().CGColor
+        paddle.layer.borderWidth = 2.0
+        paddle.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        paddle.layer.shadowOpacity = 0.5
+        
+        self.gameView.addSubview(paddle)
+        
+        return paddle
+    }()
     
     // MARK: - Livecycle
     
@@ -29,6 +48,13 @@ class ViewController: UIViewController {
         animator.addBehavior(breakout)
         
         gameView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "pushBall:"))
+        gameView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "panPaddle:"))
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: "swipePaddleLeft:")
+        swipeLeft.direction = .Left
+        gameView.addGestureRecognizer(swipeLeft)
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "swipePaddleRight:")
+        swipeRight.direction = .Right
+        gameView.addGestureRecognizer(swipeRight)
     }
     
     override func viewDidLayoutSubviews() {
@@ -37,6 +63,10 @@ class ViewController: UIViewController {
         var rect = gameView.bounds
         rect.size.height *= 2
         breakout.addBarrier(UIBezierPath(rect: rect), named: Constants.BoxPathName)
+        
+        if !CGRectContainsRect(gameView.bounds, paddle.frame) {
+            resetPaddle()
+        }
         
         for ball in breakout.balls {
             if !CGRectContainsRect(gameView.bounds, ball.frame) {
@@ -49,13 +79,15 @@ class ViewController: UIViewController {
     // MARK: - ball
     
     func placeBall(ball: UIView) {
-        ball.center = CGPoint(x: gameView.bounds.midX, y: gameView.bounds.midY)
+        var center = paddle.center
+        center.y -= Constants.PaddleSize.height / 2 + Constants.BallRadius
+        ball.center = center
     }
     
     func createBall() -> UIView {
-        let ball = UIView(frame: CGRect(origin: CGPoint.zeroPoint, size: CGSize(width: Constants.BallSize, height: Constants.BallSize)))
+        let ball = UIView(frame: CGRect(origin: CGPoint.zeroPoint, size: CGSize(width: Constants.BallRadius * 2.0, height: Constants.BallRadius * 2.0)))
         ball.backgroundColor = Constants.BallColor
-        ball.layer.cornerRadius = Constants.BallSize / 2.0
+        ball.layer.cornerRadius = Constants.BallRadius
         ball.layer.borderColor = UIColor.blackColor().CGColor
         ball.layer.borderWidth = 2.0
         ball.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
@@ -74,5 +106,49 @@ class ViewController: UIViewController {
         }
     }
     
+    // MARK: - paddle
+
+    func resetPaddle() {
+        paddle.center = CGPoint(x: gameView.bounds.midX, y: gameView.bounds.maxY - paddle.bounds.height)
+        addPaddleBarrier()
+    }
+
+    func placePaddle(translation: CGPoint) {
+        var origin = paddle.frame.origin
+        origin.x = max(min(origin.x + translation.x, gameView.bounds.maxX - Constants.PaddleSize.width), 0.0)
+        paddle.frame.origin = origin
+        addPaddleBarrier()
+    }
+    
+    func addPaddleBarrier() {
+        breakout.addBarrier(UIBezierPath(roundedRect: paddle.frame, cornerRadius: Constants.PaddleCornerRadius), named: Constants.PaddlePathName)
+    }
+    
+    func panPaddle(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            placePaddle(gesture.translationInView(gameView))
+            gesture.setTranslation(CGPointZero, inView: gameView)
+        default: break
+        }
+    }
+
+    func swipePaddleLeft(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended:
+            placePaddle(CGPoint(x: -gameView.bounds.maxX, y: 0.0))
+        default: break
+        }
+    }
+
+    func swipePaddleRight(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended:
+            placePaddle(CGPoint(x: gameView.bounds.maxX, y: 0.0))
+        default: break
+        }
+    }
+
 }
 
